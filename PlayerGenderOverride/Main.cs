fc80f7e7c0deb5109854add2f2bd0f276c406b16
Aaron Maslen <aaron.maslen@gmail.com>
2024-05-27 14:46:37 +1000

    Fix settings serialization

--- PlayerGenderOverride/Main.cs
+++ PlayerGenderOverride/Main.cs
@@ -12,8 +12,10 @@ using HarmonyLib;
 
 using Kingmaker;
 using Kingmaker.Blueprints.Base;
+using Kingmaker.Blueprints.JsonSystem;
 using Kingmaker.Designers.EventConditionActionSystem.Conditions;
 using Kingmaker.ElementsSystem.ContextData;
+using Kingmaker.EntitySystem;
 using Kingmaker.EntitySystem.Persistence;
 using Kingmaker.Localization;
 using Kingmaker.Localization.Enums;
@@ -23,6 +25,7 @@ using Kingmaker.TextTools;
 using Kingmaker.TextTools.Core;
 using Kingmaker.UnitLogic.Parts;
 using Kingmaker.Utility.DotNetExtensions;
+using Kingmaker.Utility.Serialization;
 using Kingmaker.Visual.Sound;
 
 using Newtonsoft.Json;
@@ -35,42 +38,116 @@ namespace PlayerGenderOverride;
 
 static class Main
 {
+    [HarmonyPatch]
+    internal static class Settings
+    {
+        const string SettingKey = $"{nameof(PlayerGenderOverride)}";
+        const string CustomKey = $"{SettingKey}.Custom";
+        const string CustomStringsKey = $"{CustomKey}.Strings";
+
+        internal static Gender? Gender
+        {
+            set
+            {
+                if (!InGame) return;
+
+                if (value is null)
+                    SettingsController.Instance.InSaveSettingsProvider.RemoveKey(SettingKey);
+                else
+                    SettingsController.Instance.InSaveSettingsProvider.SetValue(SettingKey, value);
+            }
+
+            get
+            {
+                if (!InGame) return null;
+
+                if (!SettingsController.Instance.InSaveSettingsProvider.HasKey(SettingKey)) return null;
+
+                return SettingsController.Instance.InSaveSettingsProvider.GetValue<Gender>(SettingKey);
+            }
+        }
+
+        internal static bool UseCustom
+        {
+            get =>
+                SettingsController.Instance.InSaveSettingsProvider.HasKey(CustomKey) &&
+                (SettingsController.Instance.InSaveSettingsProvider.GetValue<bool?>(CustomKey) ?? false);
+            set
+            {
+                if (value)
+                    SettingsController.Instance.InSaveSettingsProvider.SetValue(CustomKey, true);
+                else if (SettingsController.Instance.InSaveSettingsProvider.HasKey(CustomKey))
+                    SettingsController.Instance.InSaveSettingsProvider.RemoveKey(CustomKey);
+            }
+        }
+
+        static Dictionary<string, string>? customStrings = null;
+
+        [HarmonyPatch(typeof(ThreadedGameLoader), nameof(ThreadedGameLoader.DeserializeInGameSettings))]
+        [HarmonyPrefix]
+        static void ResetCustomStrings()
+        {
+#if DEBUG
+            Main.Logger.Log("Reset custom strings");
+#endif
+            customStrings = null;
+        }
+
+        internal static Dictionary<string, string> CustomStrings
+        {
+            get
+            {
+                if (SettingsController.Instance.InSaveSettingsProvider.HasKey(CustomStringsKey) &&
+                    customStrings is null &&
+                    SettingsController.Instance.InSaveSettingsProvider.GetValue<string>(CustomStringsKey) is string json)
+                {
+#if DEBUG
+                    Main.Logger.Log($"Load custom strings: {json}");
+#endif
+
+                    customStrings = Json.Serializer.DeserializeObject<Dictionary<string, string>>(json) ?? [];
+                }
+
+                return customStrings ??= [];
+            }
+
+            set
+            {
+                if (value is null || value.Empty())
+                {
+                    customStrings = [];
+
+                    if (SettingsController.Instance.InSaveSettingsProvider.HasKey(CustomStringsKey))
+                        SettingsController.Instance.InSaveSettingsProvider.RemoveKey(CustomStringsKey);
+
+                    return;
+                }
+
+                customStrings = value;
+
+                var json = Json.Serializer.SerializeObject(customStrings);
+
+#if DEBUG
+                Main.Logger.Log($"Save custom strings: {json}");
+#endif
+
+                SettingsController.Instance.InSaveSettingsProvider.SetValue(CustomStringsKey, json);
+            }
+        }
+    }
+
     private static Harmony? harmonyInstance;
     internal static Harmony HarmonyInstance { get => harmonyInstance!; set => harmonyInstance = value; }
     
     private static UnityModManager.ModEntry.ModLogger? log;
     internal static UnityModManager.ModEntry.ModLogger Logger => log!;
 
-    internal const string SettingKey = $"{nameof(PlayerGenderOverride)}";
-    internal const string CustomKey = $"{SettingKey}.Custom";
-    internal const string CustomStringsKey = $"{CustomKey}.Strings";
-
+    
     static bool InGame =>
         !Game.IsInMainMenu &&
         Game.Instance.SaveManager.CurrentState is not SaveManager.State.Loading &&
+        //Game.Instance.UILoadingProgress >= 1.0 &&
         Game.Instance?.Player is not null;
-
-    internal static Gender? Gender
-    {
-        set
-        {
-            if (!InGame) return;
-
-            if (value is null)
-                SettingsController.Instance.InSaveSettingsProvider.RemoveKey(SettingKey);
-            else
-                SettingsController.Instance.InSaveSettingsProvider.SetValue(SettingKey, value);
-        }
-
-        get
-        {
-            if (!InGame) return null;
-
-            if (!SettingsController.Instance.InSaveSettingsProvider.HasKey(SettingKey)) return null;
-
-            return SettingsController.Instance.InSaveSettingsProvider.GetValue<Gender>(SettingKey);
-        }
-    }
 
     static (Locale, string[]) customKeys = ((Locale)(-1), []);
     static (int, int) progress = (0, 0);
@@ -148,20 +225,6 @@ static class Main
         return true;
     }
 
-    internal static bool UseCustom
-    {
-        get =>
-            SettingsController.Instance.InSaveSettingsProvider.HasKey(CustomKey) &&
-            (SettingsController.Instance.InSaveSettingsProvider.GetValue<bool?>(CustomKey) ?? false);
-        set
-        { 
-            if (value)
-                SettingsController.Instance.InSaveSettingsProvider.SetValue(CustomKey, true);
-            else if (SettingsController.Instance.InSaveSettingsProvider.HasKey(CustomKey))
-                SettingsController.Instance.InSaveSettingsProvider.RemoveKey(CustomKey);
-        }
-    }
-
     static string ExportPath =>
         Path.Combine(
             Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
@@ -193,14 +256,14 @@ static class Main
             {
                 GUILayout.Label("Force gender:");
 
-                int selected = Gender is null ? 0 : ((int)Gender + 1);
+                int selected = Settings.Gender is null ? 0 : ((int)Settings.Gender + 1);
 
                 selected = GUILayout.SelectionGrid(selected, ["Disabled", "Male", "Female"], 3, "toggle");
 
-                Gender = selected switch
+                Settings.Gender = selected switch
                 {
-                    1 => Kingmaker.Blueprints.Base.Gender.Male,
-                    2 => Kingmaker.Blueprints.Base.Gender.Female,
+                    1 => Gender.Male,
+                    2 => Gender.Female,
                     _ => null
                 };
 
@@ -208,40 +271,39 @@ static class Main
             }
             GUILayout.EndHorizontal();
             
-            if (Gender == null)
+            if (Settings.Gender == null)
             {
-                UseCustom = false;
+                Settings.UseCustom = false;
                 GUI.enabled = false;
             }
 
             GUILayout.BeginHorizontal();
             {
-                UseCustom = GUILayout.Toggle(UseCustom, "Custom text");
+                Settings.UseCustom = GUILayout.Toggle(Settings.UseCustom, "Custom text");
 
-                GUI.enabled = UseCustom;
+                GUI.enabled = Settings.UseCustom;
                 if (GUILayout.Button("Export"))
                 {
                     Export(customMap);
                 }
                 
-                GUI.enabled = UseCustom && File.Exists(ExportPath);
+                GUI.enabled = Settings.UseCustom && File.Exists(ExportPath);
                 if (GUILayout.Button("Import"))
                 {
                     customMap = Import() ?? customMap;
                 }
-                GUI.enabled = UseCustom;
+                GUI.enabled = Settings.UseCustom;
 
                 GUILayout.FlexibleSpace();
             }
             GUILayout.EndHorizontal();
 
-            if (UseCustom)
+            if (Settings.UseCustom)
             {
                 static Dictionary<string, string>? getCustomMap()
                 {
-                    return SettingsController.Instance.InSaveSettingsProvider.HasKey(CustomStringsKey) ?
-                        SettingsController.Instance.InSaveSettingsProvider.GetValue<Dictionary<string, string>>(CustomStringsKey) :
-                        null;
+                    var value = Settings.CustomStrings;
+                    return value.Empty() ? null : value;
                 }
 
                 customMap = getCustomMap() ?? customMap;
@@ -293,11 +355,13 @@ static class Main
                     GUILayout.EndHorizontal();
                 }
 
-                SettingsController.Instance.InSaveSettingsProvider.SetValue(CustomStringsKey, customMap);
+                //SettingsController.Instance.InSaveSettingsProvider.SetValue(CustomStringsKey, customMap);
+                Settings.CustomStrings = customMap;
             }
             else
             {
-                SettingsController.Instance.InSaveSettingsProvider.RemoveKey(CustomStringsKey);
+                //SettingsController.Instance.InSaveSettingsProvider.RemoveKey(CustomStringsKey);
+                Settings.CustomStrings = [];
             }
         }
         GUILayout.EndVertical();
@@ -308,7 +372,7 @@ static class Main
 [HarmonyPatch]
 static class GenderPatch
 {
-    static Gender GenderOverride(Gender pcGender) => Main.Gender ?? pcGender;
+    static Gender GenderOverride(Gender pcGender) => Main.Settings.Gender ?? pcGender;
 
     [HarmonyPatch(typeof(SoundUtility), nameof(SoundUtility.SetGenderFlags))]
     [HarmonyTranspiler]
@@ -336,10 +400,16 @@ static class GenderPatch
     [HarmonyPrefix]
     static void BaseTextTemplateEngine_Process_Patch(ref string text)
     {
-        if (!Main.UseCustom || !SettingsController.Instance.InSaveSettingsProvider.HasKey(Main.CustomStringsKey))
+        if (!Main.Settings.UseCustom || Main.Settings.CustomStrings.Empty())
             return;
 
-        var customMap = SettingsController.Instance.InSaveSettingsProvider.GetValue<Dictionary<string, string>>(Main.CustomStringsKey);
+        var customMap = Main.Settings.CustomStrings;
+
+        if (customMap is null)
+        {
+            Main.Logger.Error("Failed to load custom strings");
+            return;
+        }
 
         foreach (var pair in customMap)
         {
@@ -369,18 +439,18 @@ static class GenderPatch
     [HarmonyPostfix]
     static bool IsFemale_Patch(bool __result)
     {
-        if (Main.Gender is null) return __result;
+        if (Main.Settings.Gender is null) return __result;
 
-        return Main.Gender == Gender.Female;
+        return Main.Settings.Gender == Gender.Female;
     }
 
     [HarmonyPatch(typeof(PcMale), nameof(PcMale.CheckCondition))]
     [HarmonyPostfix]
     static bool IsMale_Patch(bool __result)
     {
-        if (Main.Gender is null) return __result;
+        if (Main.Settings.Gender is null) return __result;
 
-        return Main.Gender == Gender.Male;
+        return Main.Settings.Gender == Gender.Male;
     }
 }
 
